75ad704a1e69e333416d825437453f2497acb827
Kevin Lin <kevinlinxc@gmail.com>
2025-02-17 19:31:47 -0800

    Try vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'PokéRogue Path Finder',
@@ -36,6 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   )
 }
